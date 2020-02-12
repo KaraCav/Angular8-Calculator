9a9c641ad9edc03fdd2b2fcfb1c5ea4ac791b441
KaraCav <karacav@msn.com>
2020-02-11 21:56:05 -0500

    can use operators on calculator

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,101 +1,66 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Operator } from 'rxjs';
 @Component({
   selector: 'app-calculator',
   templateUrl: './calculator.component.html',
   styleUrls: ['./calculator.component.scss']
 })
 export class CalculatorComponent implements OnInit {
-   displayNumber= '0';
-  // integerNumber1 = 0;
-  // integerNumber2 = 0;
-  // firstOperand = null;
-  // operator = null;
-  // waitForSecondNumber = false;
-  userInputString = "";
-  isOperator: boolean;
-  operatorSelected = [];
-
-  public isAnOperator(userInput: string) {
-    this.isOperator = (userInput === "+" || userInput === "-" || userInput === "*" || userInput === "/")
-    return this.isOperator;
-  }
-  constructor() { }
-
-public getInput(userInput: string) {
-  let isEqualToZero= false;
-  if(this.displayNumber === '0') {
-      isEqualToZero = true;
-      this.displayNumber = userInput;
-      this.userInputString = userInput;
+  displayNumber= '0';
+  integerNumber1 = null;
+  integerNumber2 = null;
+  operator = null;
+  nextOperator = null;
+  answer = null;
+  getNumber(input:string) {
+    if(this.displayNumber === '0') {
+      this.displayNumber = input;
     }
-  else {
-    this.userInputString = this.userInputString + userInput;
-  }
-  if(this.isAnOperator) {
-    this.operatorSelected.push(userInput);
-  }
-  if(!this.isAnOperator(userInput) && !isEqualToZero) {
-    this.displayNumber = this.displayNumber + userInput;
-  }
-  else if (isEqualToZero){
-    this.displayNumber = "";
-  }
-}
-
-  // public getNumber(v: string) {
-  //   if(this.displayNumber === '0') {
-  //     this.displayNumber = v;
-  //   }
-  //   else {
-  //     +this.displayNumber === this.integerNumber2 ? this.displayNumber = "0" : this.displayNumber += v;
-  //   }
-  //   if(this.integerNumber2 !== 0 && this.integerNumber1 ===0) {
-  //       this.integerNumber1 = +v;
-  //   }
-  //   console.log('get number display', this.displayNumber);
-  // }
-public getOperator(v: string) {
-  this.userInputString
-}
-
-  // public getOperator(v: string) {
-  //   this.operator = v;
-  //   console.log('operator', this.operator);
-  //   if(this.integerNumber2 === 0) {
-  //   this.integerNumber2 = +this.displayNumber;    
-  //   console.log('integer 2 complete', this.integerNumber2);
-  //   }
-  //   else {
-  //     this.integerNumber1 = +this.displayNumber;  
-  //       console.log('int 1', this.integerNumber1);
-  //     }
-  //     if(this.integerNumber1 !==0 && this.integerNumber2 !== 0) {
-  //       if(this.operator === "+") {
-  //         this.integerNumber2 = this.integerNumber1 + this.integerNumber2;
-  //         console.log('final answer', this.integerNumber2);
-  //       }
-  //       this.displayNumber = this.integerNumber2 + "";
-  //     }
-  // }
-  
-  public allClear() {
-    //this.integerNumber1 = 0;
-    this.displayNumber = '0';
-  }
-
-   public itEquals() {
-     console.log(this.userInputString);
-   let myVar = this.userInputString.split(/[^0-9]/);
-    console.log(myVar);
-    for (let i = 0; i < myVar.length; i++) {
-
-      this.operatorSelected[i];
+    else {
+      this.displayNumber = this.displayNumber + input;
     }
+    this.integerNumber1 = +this.displayNumber;
   }
-
-
+  getOperator(input:string) {
+    if(this.integerNumber2 === null) {
+    //  this.operator = input;
+      this.integerNumber2 = this.integerNumber1;
+      this.integerNumber1 = null;
+      this.displayNumber = '0';
+    }
+    else {
+      this.getAnswer();
+    //  this.operator = input;
+     // this.nextOperator = input;
+      console.log(this.integerNumber1,input, this.integerNumber2, "=", this.answer);
+    }
+    this.operator = input;
+  }
+  getAnswer() {
+    switch(this.operator) { 
+      case '+':
+        this.answer = this.integerNumber2 + this.integerNumber1;
+        break;
+      case '-':
+        this.answer = this.integerNumber2 - this.integerNumber1;
+        break;
+      case '*':
+        this.answer = this.integerNumber2 * this.integerNumber1;
+        break;
+      case '/':
+          this.answer = this.integerNumber2 / this.integerNumber1;
+    }
+    this.displayNumber = this.answer;
+    this.integerNumber1 = null;
+    this.integerNumber2 = this.answer;
+  }
+  allClear() {
+    this.displayNumber= '0';
+    this.integerNumber1 = null;
+    this.integerNumber2 = null;
+    this.operator = null;
+    this.answer = null;
+  }
   ngOnInit() {
   }
-
 }
