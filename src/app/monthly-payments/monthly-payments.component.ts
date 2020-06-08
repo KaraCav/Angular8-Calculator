56765970111c34be105d49fe76e93ee919df8668
KaraCav <karacav@msn.com>
2020-06-08 12:59:28 -0400

    started weather section

--- src/app/monthly-payments/monthly-payments.component.ts
+++ src/app/monthly-payments/monthly-payments.component.ts
@@ -9,6 +9,20 @@ export class MonthlyPaymentsComponent implements OnInit {
 
   constructor() { }
 
+  getWeatherData() {
+fetch('./data.json')
+  .then(response => {
+    return response.json()
+  })
+  .then(data => {
+    // Work with JSON data here
+    console.log(data)
+  })
+  .catch(err => {
+    // Do something for an error here
+  })
+}
+
   ngOnInit() {
   }
 
